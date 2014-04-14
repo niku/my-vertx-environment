9ec20048c099366339d141e36820e53afafde05a
niku <niku@niku.name>
2014-04-15 03:09:00 +0900

    Launch vertx module

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "base"
+  config.vm.network :forwarded_port, guest: 8080, host: 18080
   config.vm.provision 'chef_solo' do |chef|
     chef.cookbooks_path = ['vendor/cookbooks', 'site-cookbooks']
     chef.data_bags_path = 'data_bags'
--- site-cookbooks/vertx-gradle-template/recipes/default.rb
+++ site-cookbooks/vertx-gradle-template/recipes/default.rb
@@ -6,7 +6,25 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-git 'vertx-gradle-template' do
+git '/home/vagrant/vertx-gradle-template' do
   repository 'https://github.com/vert-x/vertx-gradle-template.git'
+  user 'vagrant'
+  group 'vagrant'
   action :sync
+end
+
+execute 'launch module' do
+  user 'vagrant'
+  group 'vagrant'
+  command <<-EOC
+/sbin/start-stop-daemon \
+--start \
+--background \
+--oknodo \
+--user vagrant \
+--group vagrant \
+--chdir /home/vagrant/vertx-gradle-template \
+--exec /home/vagrant/vertx-gradle-template/gradlew \
+-- assemble runMod
+EOC
 end
